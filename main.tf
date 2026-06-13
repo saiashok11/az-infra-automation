9f29cead4ff649b7e8e4c5f664af1b575c864aab
saiashok11 <111910010+saiashok11@users.noreply.github.com>
2026-06-13 21:52:07 +0530

    Fix location reference in virtual network resource

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ resource "azurerm_resource_group" "rg01" {
 resource "azurerm_virtual_network" "vnet01" {
   name                = var.virtual_network_name
   address_space       = var.address_space
-  location            = azurerm_resource_group.vnet01.location
+  location            = azurerm_resource_group.rg01.location
   resource_group_name = azurerm_resource_group.rg01.name
 }
 
